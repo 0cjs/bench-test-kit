49d8075ed4102543fb3b228e9bedb83619db5e9d
Phil Crump <phil@philcrump.co.uk>
2019-01-04 12:36:10 +0000

    leontp: Add SNTP request.

--- common.py
+++ common.py
@@ -4,6 +4,10 @@ from requests import post
 from base64 import b64encode
 from json import loads
 
+import socket
+from struct import unpack
+from time import localtime
+
 def find_arp(INSTRUMENT_OUI):
     result = subprocess.check_output("arp -n | grep '%s' | awk '{print $1}'" % (INSTRUMENT_OUI), shell=True)
     result_string = result.decode("utf-8").strip()
@@ -40,3 +44,36 @@ def imgur_post(filename, title):
         print("Uploaded: %s" % (req_data["link"]))
     else:
         print("Imgur Upload Failed: %s" % (req_data["error"]))
+
+class SNTP_Request:
+    pass
+
+def sntp_request(server='0.uk.pool.ntp.org'):
+    UNIX_NTP_DELTA = 2208988800
+
+    NTP_QUERY = bytearray(48)
+    NTP_QUERY[0] = 0x1b
+
+    addr = socket.getaddrinfo(server, 123)[0][-1]
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    res = s.sendto(NTP_QUERY, addr)
+    msg = s.recv(48)
+    s.close()
+
+    response = SNTP_Request()
+
+    response.stratum = (unpack("!B", msg[1:2])[0])
+    response.ref_id = unpack("4s", msg[12:16])
+    response.ts_ref = unpack("!I", msg[16:20])[0]
+    response.ts_orig = unpack("!I", msg[24:28])[0]
+    response.ts_rx = unpack("!I", msg[32:36])[0]
+    response.ts_tx = unpack("!I", msg[40:44])[0]
+
+    if response.stratum == 0:
+        # KoD
+        return response
+
+    response.time = localtime(response.ts_tx - UNIX_NTP_DELTA)
+    response.timestamp = "%d-%02d-%02d %02d:%02d:%02d" % (response.time.tm_year, response.time.tm_mon, response.time.tm_mday, response.time.tm_hour, response.time.tm_min, response.time.tm_sec)
+
+    return response
